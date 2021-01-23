9b4b7ce86dc752790191123c29aa168894181748
Osama Mhd <osamamhd82@gmail.com>
2021-01-23 15:15:34 +0200

    Task update virew.

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,12 @@
 from django.views.generic import ListView
-
+from django.views.generic.edit import UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Task
 
 class TaskListView(ListView):
     model = Task
     template_name = 'task_list.html'
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'task_update.html'
