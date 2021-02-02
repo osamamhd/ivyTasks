53e63580e77f0abf12e4c118a4b6d53eedd95332
Osama Mhd <osamamhd82@gmail.com>
2021-02-02 20:20:14 +0200

    taks edit and delete task path.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path 
 
-from .views import TaskListView
+from .views import (
+    TaskListView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
+    path('<int:pk>/edit/', TaskUpdateView.as_view(), name='task_update'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
