65b97d57178ec5e4b247a3b294131821894c5f40
Osama Mhd <osamamhd82@gmail.com>
2021-02-02 20:42:46 +0200

    task create view path.

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from .views import (
     TaskListView,
+    TaskCreateView,
     TaskUpdateView,
     TaskDeleteView,
 )
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
+    path('new/', TaskCreateView.as_view(), name='task_create'),
     path('<int:pk>/edit/', TaskUpdateView.as_view(), name='task_update'),
     path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
