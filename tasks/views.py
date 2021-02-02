6c8f2026bd73ca86edcd78a8f88448655bb7d384
Osama Mhd <osamamhd82@gmail.com>
2021-02-02 20:43:40 +0200

    delete detailview, add create task view.

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,27 @@
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy, reverse
 from .models import Task
 
 class TaskListView(ListView):
     model = Task
     template_name = 'task_list.html'
 
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = 'task_create.html'
+    fields = ('title', 'content')
+
+    def get_success_url(self):
+        return reverse('task_list')
+
 class TaskUpdateView(UpdateView):
     model = Task
     template_name = 'task_update.html'
     fields = ('title','content',)
+
+    def get_success_url(self):
+        return reverse('task_list')
 
 class TaskDeleteView(DeleteView):
     model = Task
