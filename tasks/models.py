d2a0bcda35e2443ceba5a333a49f8f0ba16fda3c
Osama Mhd <osamamhd82@gmail.com>
2021-02-10 18:15:24 +0200

    import AutoSlugField and add slug to Task model.

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,11 @@
 from django.utils import timezone
 from django.db import models
 from django.conf import settings
-
+from autoslug import AutoSlugField
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
+    slug = AutoSlugField(populate_from='title')
     content = models.TextField(blank=True)
     pub_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
     due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
@@ -16,7 +17,7 @@ class Task(models.Model):
         blank=True,
         on_delete=models.CASCADE,
     )
-
+    
     class Meta:
         ordering = ["-pub_date"] # Ordering by the pub date field
     def __str__(self):
