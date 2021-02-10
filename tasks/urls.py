071c66c06e07eb4b12734da25f7fb94e21abc1e9
Osama Mhd <osamamhd82@gmail.com>
2021-02-10 20:05:44 +0200

    path for completedtaskview.

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,11 +6,13 @@ from .views import (
     TaskCreateView,
     TaskUpdateView,
     TaskDeleteView,
+    CompletedTaskView,
 )
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='task_list'),
     path('new/', TaskCreateView.as_view(), name='task_create'),
+    path('completed/<slug:slug>/', CompletedTaskView.as_view(), name='task_completed'),
     path('edit/<slug:slug>/', TaskUpdateView.as_view(), name='task_update'),
     path('delete/<slug:slug>/', TaskDeleteView.as_view(), name='task_delete'),
 ]
